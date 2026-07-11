ccbd8709d516676794870ec38727d46d07af502d
absmlay <journaclass@gmail.com>
2026-07-11 14:17:35 +0300

    Day5_Boolean fastAttack implementation

---
+++ src/AnnalynsInfilitration.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Yosef S
+ */
+public class AnnalynsInfilitration {
+    public static boolean canFastAttack(boolean knightIsAwake){
+        return knightIsAwake == false;
+    }
+}
